3ff9402220c149ce7466efd02f0357942a64c572
HP <HP@DESKTOP-Q7DQ7NJ>
2024-07-24 11:15:23 +0530

    sccs

---
+++ lplp/src/lplpsdfsdf/Secoundroundofpush.java
@@ -0,0 +1,10 @@
+package lplpsdfsdf;
+
+public class Secoundroundofpush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriverManger,chromdriver.ser();
+	}
+
+}
