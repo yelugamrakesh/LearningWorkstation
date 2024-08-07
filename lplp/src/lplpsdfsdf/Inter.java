39404f0c107b9ffbec1e7cf95331a76b52925474
yelugamrakesh <31204282+yelugamrakesh@users.noreply.github.com>
2024-08-08 05:11:01 +0530

    1

---
+++ lplp/src/lplpsdfsdf/Inter.java
@@ -0,0 +1,13 @@
+package lplpsdfsdf;
+
+public class Inter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Printed You");
+		
+		
+	}
+
+}
